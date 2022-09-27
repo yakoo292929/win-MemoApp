f2474c6f1307fa1c14fe2f721b315e3c3b3414d9
yakoo292929 <yakoo292929@gmail.com>
2022-09-27 16:04:47 +0900

    Replace KeyboradAvoidingView

--- src/screens/MemoCreateScreen.jsx
+++ src/screens/MemoCreateScreen.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { View, TextInput, StyleSheet, KeyboardAvoidingView } from 'react-native';
+import { View, TextInput, StyleSheet, KeyboardAvoidingView, } from 'react-native';
 
 import AppBar from "../components/AppBar";
 import CircleButton from "../components/CircleButton";
